37f8351cb4cf853496ecb5f88bf1367eb6bd7e04
mnzit <mnzitshakya@gmail.com>
2022-06-29 19:59:07 +0545

    example abstraction

---
+++ add/AddCommand.java
@@ -0,0 +1,10 @@
+
+public class AddCommand extends MathCommand{
+
+    @Override
+    public double calculate(double x, double y) {
+       
+        return x+y;
+    }
+    
+}
---
+++ add/MathCommand.java
@@ -0,0 +1,6 @@
+
+public abstract class MathCommand {
+
+     public abstract double calculate(double x, double y);
+    
+}
