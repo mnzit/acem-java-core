e3d8bec70a80f96a0823ccee39f4b6b61177d09a
mnzit <mnzitshakya@gmail.com>
2022-07-14 16:31:17 +0545

    Scanner file reader

--- array/Main.java
+++ array/Main.java
@@ -1,51 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Set<Student> names = new LinkedHashSet<>();
+       Scanner scanner = new Scanner(new FileReader("file.txt"));
 
-        Student student1 = new Student(1L, "Biren");
-        Student student2 = new Student(1L, "Biren");
-        Student student3 = new Student(2L, "Biren");
-        boolean condition = student1.equals(student2);
-
-        System.out.println(condition);
-        names.add(student1);
-        names.add(student2);
-        names.add(student3);
-
-
-
-        System.out.println(names);
-
-
-        Queue<String> games = new PriorityQueue<>();
-
-        games.add("Fortnite");
-        games.add("PSG");
-
-        games.poll();
-
-        System.out.println(games.poll());
-
-
-        Stack<String> movies = new Stack<>();
-
-
-        movies.add("Batman");
-        movies.add("Superman");
-        movies.add("Minions");
-
-        movies.pop();
-
-        System.out.println(movies);
-
-
-    }
-
-    @Override
-    public String toString() {
-        return super.toString();
+       while(scanner.hasNext()){
+           System.out.println(scanner.nextLine());
+       }
     }
 }
