1310befbffa384adb1f378c1fa1d089a9c23b533
mnzit <mnzitshakya@gmail.com>
2022-07-19 16:47:18 +0545

    Streams -> lambda, filter, map, reduce, collector, method referencing

--- map/Main.java
+++ map/Main.java
@@ -1,35 +1,53 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    private static Object[] heroes = new String[10];
-
     public static void main(String[] args) {
-        put("superman");
-        System.out.println(get("superman"));
+
+
+        List<String> names = new ArrayList<>();
+
+        names.add("hari");
+        names.add("dari");
+        names.add("sari");
+        names.add("bari");
+        names.add("barri");
+        names.add("pari");
+
+        names.forEach(s -> System.out.println("name: " + s));
+
+        Stream<String> stream = names.stream();
+
+        Optional<String> result = stream
+                .filter(name -> name.startsWith("ba"))
+                .collect(Collectors.toList()).stream().findFirst();
+
+        System.out.println(result);
+
+
+        List<Integer> marks = List.of(1, 1, 1, 1, 1);
+        marks.forEach(Main::aasd);
+        System.out.println(marks.stream().map(i ->i+1).collect(Collectors.toList()));
+
+//        System.out.println(result2);
+
     }
 
-    public static Object get(Object obj){
-        int hashCode = getCode(obj);
-        System.out.println(hashCode);
-        if(hashCode >= heroes.length){
-            return null;
-        }else{
-            return heroes[hashCode];
-        }
+    interface MathCommand<P1, P2, R> {
+        R calculate(P1 param1, P2 param2);
     }
 
-    public static void put(Object object){
-        heroes[getCode(object)] = object;
+    // Map -> Internal of Map
+    // Streams -> lambda, filter, map, reduce, collector, method referencing
+
+    public static void aasd(Object o){
+        System.out.println(o);
     }
-
-    public static int getCode(Object object){
-        int hashCode = object.hashCode();
-        System.out.println("hashCode : "+ hashCode);
-        System.out.println("mod : "+ hashCode % heroes.length);
-        System.out.println(object+" location is: "+ Math.abs(hashCode % heroes.length));
-        return Math.abs(hashCode % heroes.length);
-    }
-
-
 }
+
+
